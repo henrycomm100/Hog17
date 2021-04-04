fe6e2413b0b4935ae97f051768f1126928a1d556
henry <henry@comm100.com>
2021-04-04 15:48:12 +0800

    using Session to speed up the execution

--- API/Wework/WeworkContacts.py
+++ API/Wework/WeworkContacts.py
@@ -3,7 +3,9 @@ import requests
 
 class WeworkContacts:
     def __init__(self):
+        self.s = requests.Session()
         self.token = self.get_token()
+        self.s.params = {"access_token": self.token}
 
     def get_token(self):
         url = 'https://qyapi.weixin.qq.com/cgi-bin/gettoken'
@@ -11,7 +13,7 @@ class WeworkContacts:
             "corpid": "ww62b65a79dc7207eb",
             "corpsecret": "gbaKXEt4FDWWXGiuYgeskVsgSE75Cciiv8Psom78dRk"
         }
-        r = requests.get(url, params=params)
+        r = self.s.get(url, params=params)
         return r.json()['access_token']
 
     def teardown(self):
@@ -19,44 +21,38 @@ class WeworkContacts:
 
     def add_contact(self, userid: str, name: str, mobile: str, department: list):
         url = f'https://qyapi.weixin.qq.com/cgi-bin/user/create'
-        params = {"access_token": self.token}
         data = {
             "userid": userid,
             "name": name,
             "mobile": mobile,
             "department": department
         }
-        r = requests.post(url, params=params, json=data)
+        r = self.s.post(url, json=data)
         return r.json()
 
     def get_contact(self, userid: str):
         url = f'https://qyapi.weixin.qq.com/cgi-bin/user/get'
         params = {
-            "access_token": self.token,
             "userid": userid
         }
-        r = requests.get(url, params=params)
+        r = self.s.get(url, params=params)
 
         return r.json()
 
     def update_contact(self, userid: str, name: str):
         url = f'https://qyapi.weixin.qq.com/cgi-bin/user/update'
-        params = {
-            "access_token": self.token
-        }
         data = {
             "userid": userid,
             "name": name
         }
-        r = requests.post(url, params=params, json=data)
+        r = self.s.post(url, json=data)
         return r.json()
 
     def delete_contact(self, userid: str):
         url = f'https://qyapi.weixin.qq.com/cgi-bin/user/delete'
         params = {
-            "access_token": self.token,
             "userid": userid
         }
-        r = requests.get(url, params=params)
+        r = self.s.get(url, params=params)
 
         return r.json()

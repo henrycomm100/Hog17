3b729afc2fc18f7d3a074aa793c8420d0ce708f3
henry <henry@comm100.com>
2021-07-11 16:38:59 +0800

    adding feishu calendar - delete

--- API/Feishu/test_calendar.py
+++ API/Feishu/test_calendar.py
@@ -16,9 +16,18 @@ class TestCalendar:
         r = self.calendar.add_calendar(self.summary, self.description)
 
         print(r['data'])
+        print(r['data']['calendar']['calendar_id'])
         assert r['code'] == 0
         assert r['msg'] == 'success'
+        return r['data']['calendar']['calendar_id']
 
         # # 数据检查
         # r = self.calendar.get_contact(self.userid)
         # assert r['name'] == self.name
+
+    def test_delete_calendar(self):
+        calendar_id = self.calendar.add_calendar(self.summary, self.description)
+        r = self.calendar.delete_calendar(calendar_id=calendar_id)
+
+        assert r['code'] == 0
+        assert r['msg'] == 'success'

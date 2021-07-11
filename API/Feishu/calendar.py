8e10961abcc1e2a55713858d6b1047a1b2b0f9d1
henry <henry@comm100.com>
2021-07-11 16:41:54 +0800

    adding feishu calendar - delete

--- API/Feishu/calendar.py
+++ API/Feishu/calendar.py
@@ -16,29 +16,30 @@ class FeishuCalendar(Base):
         r = self.send('POST', url, json=data)
         return r.json()
 
-    def get_calendar_list(self, userid: str):
-        url = f'https://open.feishu.cn/open-apis/calendar/v4/calendars'
-        r = self.send('GET', url)
-
-        return r.json()
-
-    def get_calendar(self, userid: str):
-        url = f'https://open.feishu.cn/open-apis/contact/v3/departments/:department_id'
-        params = {
-            "userid": userid
-        }
-        r = self.send('GET', url, params=params)
-
-        return r.json()
-
-    def update_calendar(self, userid: str, name: str):
-        url = f'https://open.feishu.cn/open-apis/contact/v3/departments/:department_id'
-        data = {
-            "userid": userid,
-            "name": name
-        }
-        r = self.send('POST', url, json=data)
-        return r.json()
+    #
+    # def get_calendar_list(self, userid: str):
+    #     url = f'https://open.feishu.cn/open-apis/calendar/v4/calendars'
+    #     r = self.send('GET', url)
+    #
+    #     return r.json()
+    #
+    # def get_calendar(self, userid: str):
+    #     url = f'https://open.feishu.cn/open-apis/contact/v3/departments/:department_id'
+    #     params = {
+    #         "userid": userid
+    #     }
+    #     r = self.send('GET', url, params=params)
+    #
+    #     return r.json()
+    #
+    # def update_calendar(self, userid: str, name: str):
+    #     url = f'https://open.feishu.cn/open-apis/contact/v3/departments/:department_id'
+    #     data = {
+    #         "userid": userid,
+    #         "name": name
+    #     }
+    #     r = self.send('POST', url, json=data)
+    #     return r.json()
 
     def delete_calendar(self, calendar_id: str):
         url = f'https://open.feishu.cn/open-apis/calendar/v4/calendars/:{calendar_id}'
